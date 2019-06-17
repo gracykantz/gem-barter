168dfb8378771223be058858888cffd42183bae2
Mika Bettanin <mauro.bettanin@ikea.com>
2019-06-17 14:45:03 +0200

    creating gem-category branch

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,5 @@ Rails.application.routes.draw do
     resources :swipes, except: :destroy  #only: [:index, :new, :create, :edit, :update]
     # resource :images
   end
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
