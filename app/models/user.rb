b217c987873153e0b6a38073944d0d9073e1a162
Mika Bettanin <mauro.bettanin@ikea.com>
2019-06-17 13:24:34 +0200

    added User model, controller, view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :messages
+  has_many :reviews
+  has_many :furniture_items
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
