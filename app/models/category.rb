ead2bc01ad6553159840bf51ffee39e8ff15552e
Gracy Kantzabedian <gracy.kantzabedian@ikea.com>
2019-06-18 09:39:07 +0200

    fixed seed file

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   has_many :furniture_items
   validates :name, presence: true
   validates :photo, presence: true
--- db/migrate/20190617083119_add_fields_to_user.rb
+++ db/migrate/20190617083119_add_fields_to_user.rb
@@ -3,7 +3,7 @@ class AddFieldsToUser < ActiveRecord::Migration[5.2]
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :bio, :text
-    add_column :users, :avatar, :string
+    add_column :users, :photo, :string
     add_column :users, :average_rating, :integer
     add_column :users, :location, :string
   end
