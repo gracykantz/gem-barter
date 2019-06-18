100c6caf35094f8bdfe280634d4b118985a35c87
Gracy Kantzabedian <gracy.kantzabedian@ikea.com>
2019-06-18 10:03:30 +0200

    furniture_items model updated

--- app/models/furniture_item.rb
+++ app/models/furniture_item.rb
@@ -1,8 +1,8 @@
 class FurnitureItem < ApplicationRecord
   belongs_to :user
   # has_many :swipes
-  has_one :swipe, foreign_key: 'owned_furniture_item_id', dependent: :destroy
-  has_one :swipe, foreign_key: 'wanted_furniture_item_id', dependent: :destroy
+  has_one :owner_swipe, class_name: "Swipe", foreign_key: 'owned_furniture_item_id', dependent: :destroy
+  has_one :other_swipe, class_name: "Swipe", foreign_key: 'wanted_furniture_item_id', dependent: :destroy
   has_many :images
   accepts_nested_attributes_for :images
 end
