2fbda3fe76bb74107b930fd2923327def271feb5
shalini verma <shalverma02@gmail.com>
2019-06-19 09:58:37 +0200

    Swipe record creation and corresponding item removed from the user index logic functional

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
   has_many :furniture_items
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
