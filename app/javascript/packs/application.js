9631a7615ef58dc7cadb672ff3a0fec7faf97161
Gracy Kantzabedian <gracy.kantzabedian@ikea.com>
2019-06-24 18:34:30 +0200

    updated index styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,12 @@ import "../plugins/swipemmb";
 
 
 import { initSweetalert } from '../plugins/init_sweetalert';
-
 initSweetalert('#sweet-alert-demo', {
   title: "A nice alert",
   text: "This is a great alert, isn't it?",
   icon: "success"
 });
+
 
 
     // const slider = document.querySelector('.swiper-container-h').swiper;
