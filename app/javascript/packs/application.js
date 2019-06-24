b92995b894e3cf70757f151b114568bcd7d2ebe7
Gracy Kantzabedian <gracy.kantzabedian@ikea.com>
2019-06-24 14:19:33 +0200

    minor changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,10 @@ import $ from "jquery";
 import "../plugins/swipemmb";
 
 
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+initSweetalert('#sweet-alert-demo', {
+  title: "A nice alert",
+  text: "This is a great alert, isn't it?",
+  icon: "success"
+});
