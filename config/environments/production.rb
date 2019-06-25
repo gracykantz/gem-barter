5308aab62ca825869c1990ca1c6f6a65f2bec772
Gracy Kantzabedian <gracy.kantzabedian@ikea.com>
2019-06-25 15:51:56 +0200

    change http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.action_cable.url = "wss://www.gem-bartering-made-easy.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ["http://www.gem-bartering-made-easy.herokuapp.com", "https://www.gem-bartering-made-easy.herokuapp.com"]
+  config.action_cable.allowed_request_origins = ["http://gem-bartering-made-easy.herokuapp.com", "https://gem-bartering-made-easy.herokuapp.com"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
