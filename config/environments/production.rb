211b70bcd97df0b4a8123aca68e6cc7f6143430e
Gracy Kantzabedian <gracy.kantzabedian@ikea.com>
2019-06-25 14:06:25 +0200

    change url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.action_cable.url = "wss://www.gem-bartering-made-easy.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = [ "http://www.gem-bartering-made-easy.herokuapp.com", "https://www.gem-bartering-made-easy.herokuapp.com" ]
+  config.action_cable.allowed_request_origins = [ "http://www.gem-bartering-made-easy.herokuapp", "https://www.gem-bartering-made-easy.herokuapp.com" ]
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
