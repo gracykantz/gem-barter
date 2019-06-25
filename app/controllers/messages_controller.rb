79b5098b9bdd37c1c15abc09f0af9521a793751b
Gracy Kantzabedian <gracy.kantzabedian@ikea.com>
2019-06-25 12:06:06 +0200

    fixed actioncable to hopefully work on heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,6 +9,7 @@ class MessagesController < ApplicationController
     @message.chat_room = @chat_room
     @message.user = current_user
     if @message.save!
+
       respond_to do |format|
         format.html { redirect_to chat_room_path(@chat_room) }
         format.js
