250578fac7f3ebb95276be174d4e86dfbbcfe5d0
Mika Bettanin <mauro.bettanin@ikea.com>
2019-06-26 16:50:00 +0200

    rating page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,26 @@
 class ReviewsController < ApplicationController
-  def create
-    @review = current_user.reviews.build(review_params)
-    @reviews = Review.where("user_id = ?", params[:product_id])
+  def new
+    @review = Review.new
+    @chatroom = ChatRoom.find(params[:chat_room_id])
+  end
 
+  def create
+    @review = Review.new(review_params)
+    @user_reviewed = Message.where(["chat_room_id = ? and user_id <> ?", params[:chat_room_id],  current_user.id]).first
+    @review.user_reviewed_id = @user_reviewed.id
+    @review.user_reviewing_id = current_user.id
+    redirect_to traded_path if @review.review.nil?
     if @review.save
-      redirect_to user_path(@review.user) # review_path(@review)
+      redirect_to traded_path
+    else
+      flash[:alert] = "Something went wrong."
+      render :new
     end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:review, :user_id)
+    params.require(:review).permit(:review)
   end
 end
