a4479b90c81fedf42b82d551ee4a154369b8fac1
Gracy Kantzabedian <gracy.kantzabedian@ikea.com>
2019-06-27 09:57:32 +0200

    fixed the scrolling issue with categories, and changed a few things in reviews that we can revert

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,11 +5,13 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    verify_traded
     @review = Review.new(review_params)
     @user_reviewed = Message.where(["chat_room_id = ? and user_id <> ?", params[:chat_room_id],  current_user.id]).first
-    @review.user_reviewed_id = @user_reviewed.id
-    @review.user_reviewing_id = current_user.id
+
     redirect_to traded_path if @review.review.nil?
+      @review.user_reviewed_id = @user_reviewed.id
+      @review.user_reviewing_id = current_user.id
     if @review.save
       redirect_to traded_path
     else
@@ -19,6 +21,21 @@ class ReviewsController < ApplicationController
   end
 
   private
+
+  def verify_traded
+    furniture_item = FurnitureItem.find_by_user_id(current_user.id)
+    if furniture_item.nil?
+      @trade = 'a'
+    elsif !furniture_item.matched_to_id.nil?
+      @trade = 'e'
+      @match = Match.find_by(id: furniture_item.matched_to_id)
+      if @match.traded == true
+        @trade = 't'
+      elsif @match.present?
+        @trade = 'm'
+      end
+    end
+  end
 
   def review_params
     params.require(:review).permit(:review)
